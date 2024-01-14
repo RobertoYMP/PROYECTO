91a5eca282196f83ff6767086da02830ace81dfb
D4ncg2510 <108159079+D4ncg2510@users.noreply.github.com>
2024-01-14 00:54:47 -0600

    papap

--- Principalp/Principalp/Principal/src/main/java/com/mycompany/principal/StmtExpression.java
+++ Principalp/Principalp/Principal/src/main/java/com/mycompany/principal/StmtExpression.java
@@ -5,9 +5,15 @@ public class StmtExpression extends Statement {
 
     StmtExpression(Expression expression) {
         this.expression = expression;
+        
     }
 
+
+    Object ejecutar(Tabla tabla){
+        analyzeExpression(expressionStatement.getExpression(), localScope);
+    }
     StmtExpression(java.beans.Expression expr) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
+
 }
